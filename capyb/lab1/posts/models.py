86cc570bebb31d393dd7042dfbe83f866f987947
Dmitry <dmitreyd287@gmail.com>
2024-12-21 02:56:41 +0600

    итог 4 лаба вроде

--- capyb/lab1/posts/models.py
+++ capyb/lab1/posts/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
     banner = models.ImageField(default='fallback.png', blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)  # Добавлен автор поста
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)  
 
     def __str__(self):
         return self.title
